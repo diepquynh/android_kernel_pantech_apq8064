5214096201ef2fecbe434dca31fd3c77b85dafe4
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-28 05:25:30 +0000

    build.sh: Add new build script
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

---
+++ kernel_zip/tools/flash_kernel.sh
@@ -0,0 +1,20 @@
+#!/sbin/sh
+#
+# Live ramdisk patching script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+cd /tmp/
+/sbin/busybox dd if=/dev/block/mmcblk0p7 of=./boot.img
+./unpackbootimg -i /tmp/boot.img
+./mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 loglevel=0 vmalloc=0x16000000 maxcpus=2 androidboot.selinux=permissive" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o /tmp/newboot.img
+/sbin/busybox dd if=/tmp/newboot.img of=/dev/block/mmcblk0p7
